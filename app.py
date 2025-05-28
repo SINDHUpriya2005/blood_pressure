6d530eeb29238bdf5f003043fe8ec7e197d4c1c1
SINDHUpriya2005 <144546706+SINDHUpriya2005@users.noreply.github.com>
2025-05-28 15:40:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pickle
+import pandas as pd
+from flask import Flask, request, render_template
+
+app = Flask(__name__, static_folder='static')
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict-form')
+def predict_form():
+    return render_template('predict_form.html')
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    try:
+        data = [float(request.form[field]) for field in [
+            "Gender", "Age", "Patient", "Severity", "BreathShortness",
+            "VisualChanges", "NoseBleeding", "Whendiagnoused", 
+            "Systolic", "Diastolic", "ControlledDiet"
+        ]]
+        df = pd.DataFrame([data], columns=[
+            'Gender', 'Age', 'Patient', 'Severity', 'BreathShortness',
+            'VisualChanges', 'NoseBleeding', 'Whendiagnoused',
+            'Systolic', 'Diastolic', 'ControlledDiet'
+        ])
+
+        prediction = model.predict(df)[0]
+
+        result_map = {
+            0: "NORMAL",
+            1: "HYPERTENSION (Stage-1)",
+            2: "HYPERTENSION (Stage-2)",
+            3: "HYPERTENSIVE CRISIS"
+        }
+        result = result_map.get(prediction, "Unknown")
+
+        return render_template("result.html", prediction_text=f"Your Blood Pressure stage is: {result}")
+
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
